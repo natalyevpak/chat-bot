eeb7861934f7b1f852e8fb528f2e718240f3c362
evna <evna@gurtam.com>
2025-01-30 17:26:42 +0200

    Fixed missing icons issue

--- src/wialon-chatbot.js
+++ src/wialon-chatbot.js
@@ -12,5 +12,16 @@ const loadChatBot = () => {
   app.mount('#wialon-chatbot');
 };
 
+const loadCSS = (href) => {
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = href;
+  document.head.appendChild(link);
+};
+
+// Load PrimeIcons
+loadCSS('https://unpkg.com/primeicons/primeicons.css');
+
+
 // Automatically load the chatbot when the script is loaded
 loadChatBot();
